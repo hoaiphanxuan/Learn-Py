63bdae5adf140a5188ab95d9374881acbf7c854b
HoaiPhan <huyvongmongmanh75@gmail.com>
2021-11-14 23:51:15 +0700

    ahh

---
+++ b1.py
@@ -0,0 +1,60 @@
+# print('hoai')
+# x=1
+
+
+# if (1<=x-1<3):
+#     print('true')
+# else:
+#     print("flase")
+
+
+# list=["hoai","loi","dai"]
+# for i in list:
+#     print(i)
+
+
+# def giaiThua (a):
+#     i=1
+#     gt=1
+#     while(i<=a):
+#         gt*=i
+#         i+=1
+#     return gt
+
+# print("Giai thua cua 4 la:"+str(giaiThua(4)))
+
+
+# first_name="hoai"
+# last_name="phan xuan"
+# name=last_name+ " " + first_name
+# print(name)
+
+# paragrap="""phan xuan hoai
+# phan xuan dai
+# phan xuan loi"""
+
+# print(paragrap)
+
+# print(name[1:4])
+
+# li=name.split(" ")
+# print(li)
+
+# #viet in
+# print(paragrap.upper())
+
+# #resverse
+# print(paragrap[::-1])
+
+# #viet hoa chu dau
+# print(name.capitalize())
+
+arr=[1,4]
+for i in arr:
+    print(type(arr))
+
+#tuples
+col=("nau",'vang','cam','do')
+col=col.__add__(('xanh','hong'))
+
+print (col)
